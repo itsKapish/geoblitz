8834a42e91333d0019ea98d99f6e7a311467ded0
KapishS <84273328+itsKapish@users.noreply.github.com>
2024-04-11 11:53:34 +0530

    Add files via upload

--- EnvironmentQuiz/gbe1.js
+++ EnvironmentQuiz/gbe1.js
@@ -12,89 +12,89 @@ let startScreen = document.querySelector(".start-screen");
 let startButton = document.getElementById("start-button");
 let questionCount;
 let scoreCount = 0;
-let count = 11; 
+let count = 31; 
 let countdown;
 var pos = 0;
 //Questions and Options array
 const quizArray = [ 
     { 
         id: "0",
-        question: "Which country does this flag belong to?",
-        options: ["Czech Republic", "Slovenia", "Croatia", "Costa Rica"],
-        correct: "Czech Republic",
-        img: "czef.png", 
+        question: "A Western European country is slowly transforming into a destert. The United Nations estimates that 74% of this country is currently undergoing desertification. A severe water deficit coupled with long-lasting droughts has resulted in food being more expensive. Which country is this?",
+        options: ["Spain", "Andorra", "Portugal", "France"],
+        correct: "Spain",
+        img: "epg5.png", 
     },
     
     {
         id: "1",
-        question: "Which country does this flag belong to?",
-        options: ["Belarus", "Afghanistan", "Albania", "Kyrgyzstan"],
-        correct: "Belarus",
-        img: "belf.png",
+        question: "The Planet is warming up at an alarming rate, soon reaching a point to a catastrophic bleaching event for coral reefs located in the Great Barrier Reef. However, in a particular ocean, there is hope for marine ecosystems as a coral superhighway is forming due to ocean currents carrying larvae which are forming new coral reefs in an area spread over a million square kilometres. Which ocean is this?",
+        options: ["Indian Ocean", "Pacific Ocean", "Atlantic Ocean", "Arctic Ocean"],
+        correct: "Indian Ocean",
+        img: "epg7.png",
     },
 
     {
         id: "2",
-        question: "Which country does this flag belong to?",
-        options: ["Poland", "Uganda", "Montenegro", "Macedonia"],
-        correct: "Macedonia",
-        img: "macf.png",
+        question: "Since 1880, the average global temperature has increased by about 1 degrees Celsius. What is the projected rise in average global temperature since 1880 for 2050 (in degrees Celsius)?",
+        options: ["2.5", "3", "2", "1.5"],
+        correct: "1.5",
+        img: "epg8.png",
     },
 
     {
         id: "3",
-        question: "Which country does this flag belong to?",
-        options: ["Lebanon", "Israel", "Turkey", "Malta"],
-        correct: "Lebanon",
-        img: "lebf.png",
+        question: "As human activities keep devastating the environment, the IUCN Red List established in 1964 helps us to understand on how large a scale are human activities impacting our ecosystems. What does the IUCN Red List represent?",
+        options: ["A measure of Air Pollution in cities around the World", "A list of Threatened Forests around the World", "A list of major Natural disasters as a result of Human activities", "A list of Threatened, Endangered and Extinct Species"],
+        correct: "A list of Threatened, Endangered and Extinct Species",
+        img: "epg4.png",
     },
 
     {
         id: "4",
-        question: "Which country does this flag belong to?",
-        options: ["Fiji", "Kiribati", "Tuvalu", "Cabo Verde"],
-        correct: "Kiribati",
-        img: "kirf.png",
+        question: "The 6th Annual World Air Quality Report of the world's most polluted regions in 2023, published by IQAir, an organization in Switzerland reported that the most polluted metropolitan area of 2023 was in India. Which city was this?",
+        options: ["New Delhi", "Begusarai", "Bangalore", "Dispur"],
+        correct: "Begusarai",
+        img: "epg6.png",
     },
 
     {
         id: "5",
-        question: "Which country does this flag belong to?",
-        options: ["Cambodia", "Thailand", "North Korea", "Philippines"],
-        correct: "Philippines",
-        img: "phif.png",
+        question: "In California, many scientists are debating over the existence of a potential river in the atmosphere which could be a scientifically plausible explanation for the massive floods in California. What is the name given to this major atmospheric body of water?",
+        options: ["The Californian MegaStorm", "Californian MegaRiver", "The Californian Flood", "ARkStorm"],
+        correct: "ARkStorm",
+        img: "epg3.png",
     },
 
     {
         id: "6",
-        question: "Which country does this flag belong to?",
-        options: ["Sri Lanka", "Vatican City", "Bhutan", "Brunei"],
-        correct: "Brunei",
-        img: "bruf.png",
+        question: "Sand Batteries are amongst the many technological innovations created to help the environment and the balance of ecosystems. These are a type of Thermal Batteries used to store thermal energy at high temperatures which is generated from Solar Energy when sunlight is present during the Summer Months and used to heat homes during the Winter Months. In which country firstly has this been successfully implemented?",
+        options: ["Russia", "Finland", "Canada", "Denmark"],
+        correct: "Finland",
+        img: "epg9.png",
     },
 
     {
         id: "7",
-        question: "Which country does this flag belong to?",
-        options: ["Comoros", "Maldives", "Seychelles", "Mauritius"],
-        correct: "Seychelles",
-        img: "seyf.png",
+        question: "Lithium-ion batteries, which are rechargeable might appear to be a sustainable replacement for Lead-acid batteries at first, however they require large amounts of water, mining and extraction which damages the environment. These batteries also require Cobalt and other metals, which have to be mined for in forest habitats. Which continent contains the highest reserves of Lithium and produces the most Lithium?",
+        options: ["Asia", "North America", "South America", "Oceania"],
+        correct: "South America",
+        img: "epg2.png",
     },
 
     {
         id: "8",
-        question: "Which country does this flag belong to?",
-        options: ["El Salvador", "Guatemala", "Argentina", "Honduras"],
-        correct: "Guatemala",
-        img: "guaf.png",
+        question: "A new method of reducing Carbon dioxide emissions in the Atmosphere is Carbon Capture. This method aims to capture Carbon Dioxide from the atmosphere and store it, but where does this method aim to store Carbon Dioxide?",
+        options: ["Outer Space", "Below Ice Sheets", "Rock Formations Underground", "Deep Trenches in Oceans"],
+        correct: "Rock Formations Underground",
+        img: "epg10.png",
     },
 
     {
         id: "9",
-        question: "Which country does this flag belong to?",
-        options: ["Nicaragua", "Argentina", "Somalia", "Urugauy"],
-        correct: "Argentina",
-        img: "argf.png",
+        question: "A collaboration between two organizations, WWF and Intel, in 2018 has deployed Artificial Intelligence to protect wild tigers and their habitats, by monitoring tigers. In which country has this been implemented in?",
+        options: ["India", "China", "Vietnam", "Cambodia"],
+        correct: "China",
+        img: "epg1.png",
     },
 ];
 //Restart Quiz
@@ -123,7 +123,7 @@ nextBtn.addEventListener(
                 questionCount + 1 + " of " + quizArray.length + " Question";
             //display quiz
             quizDisplay(questionCount);
-            count = 11;
+            count = 31;
             clearInterval(countdown);
             timerDisplay();
         }
@@ -211,7 +211,7 @@ function initial() {
     quizContainer.innerHTML = "";
     questionCount = 0;
     scoreCount = 0; 
-    count = 11;
+    count = 31;
     clearInterval(countdown);
     timerDisplay();
     quizCreator();
